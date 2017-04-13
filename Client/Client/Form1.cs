95e9110c7518acdd3eb30cac14452b5b971d9fb1
pai-55 <pai-55@sr.dmcs.pl>
2017-04-13 16:47:51 +0200

    Client is able to send txt msg to the server. Both of the projects are in same solution. Multiple projects run has been set.

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox1.Text.ToString() + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[1024];
+            label1.Text = Convert.ToString((int)clientSocket.ReceiveBufferSize);
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg("Data from Server : " + returndata);
+        }
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+    }
+}
